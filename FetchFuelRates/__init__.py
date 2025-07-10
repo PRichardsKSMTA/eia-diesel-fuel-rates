503a8ab845bd078b135ffcb82ce33d518b1298c4
Pete Richards <pete.richards@ksmcpa.com>
2025-07-10 08:35:06 -0600

    Update init file to send date in iso format

--- FetchFuelRates/__init__.py
+++ FetchFuelRates/__init__.py
@@ -3,20 +3,19 @@ import datetime as dt
 import azure.functions as func
 from fetch_fuel_rates import main as fetch_main
 
-
 def get_prev_week_and_month():
     today = dt.date.today()
     # Last week’s Monday (start of the week for API)
     prev_monday = today - dt.timedelta(days=today.weekday() + 7)
-    weekly_start = prev_monday.strftime("%Y-%m-%d")
+    weekly_start = prev_monday.isoformat()      # e.g. "2025-06-22"
 
     # First day of last month
     first_this_month = today.replace(day=1)
-    last_month_last_day = first_this_month - dt.timedelta(days=1)
-    monthly_start = last_month_last_day.strftime("%Y-%m")
+    last_month = first_this_month - dt.timedelta(days=1)
+    first_last_month = last_month.replace(day=1)
+    monthly_start = first_last_month.isoformat()  # e.g. "2025-05-01"
 
     return weekly_start, monthly_start
-
 
 def main(timer: func.TimerRequest) -> None:
     # Allow override for testing
@@ -30,3 +29,9 @@ def main(timer: func.TimerRequest) -> None:
     weekly_start, monthly_start = get_prev_week_and_month()
     fetch_main(weekly_start, dry_run=False)
     fetch_main(monthly_start, dry_run=False)
+
+# To test locally with an override:
+#   Set environment variable START_DATE_OVERRIDE to "2024-01-01"
+# Then run: func start
+# Or manually invoke via:
+#   curl -X POST http://localhost:7071/admin/functions/FetchFuelRates -d '{}'
